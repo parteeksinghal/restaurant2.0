73a837dc8600ecbf9c1fdbff93e1a6934498a78e
parteeksinghal <parteek.singhal@telekom-digital.com>
2019-06-26 12:28:01 +0530

    available tables logic

--- src/main/java/com/mongo/controller/RestaurantController.java
+++ src/main/java/com/mongo/controller/RestaurantController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -22,6 +24,24 @@ public class RestaurantController {
     @RequestMapping("/test")
     public String test() {
         return "Working!";
+    }
+
+    @RequestMapping("/tables")
+    public List<Boolean> availableTables() {
+        List<Table> tables = restRepository.findAll();
+        List<Boolean> available = new ArrayList<Boolean>(Arrays.asList(false,false,false,false,false,false,false,false,false,false));
+
+        for(int i = 0; i<10; i++) {
+
+            for(int j=0; j<10; j++) {
+                if (tables.get(j).getBooked().get(i) == false) {
+                    available.set(i, true);
+                    break;
+                }
+            }
+        }
+
+        return available;
     }
 
 }
